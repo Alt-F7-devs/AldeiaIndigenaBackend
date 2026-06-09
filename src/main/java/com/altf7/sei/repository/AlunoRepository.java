ab0f24b5b36f30aadc92994a17a7d1a921304d8e
geração de token para login e autenticação de usuarios para cadastro de alunos e professores <kauanfinger1@gmail.com>
2026-06-08 21:20:44 -0300

    fix: adição do @query para realizar a pesquisa do aluno na sala

--- src/main/java/com/altf7/sei/repository/AlunoRepository.java
+++ src/main/java/com/altf7/sei/repository/AlunoRepository.java
@@ -2,6 +2,7 @@ package com.altf7.sei.repository;
 
 import com.altf7.sei.entity.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+    @Query("SELECT a FROM Aluno a WHERE a.sala.id_sala = :id_sala")
     List<Aluno> findBySala_IdSala(Integer id_sala);
     Optional<Aluno> findByCgm(String cgm);
 }
